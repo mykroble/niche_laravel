69f3ae96b2d32ee0deadf3f361befe4f2b3642dc
Shion Suzuki <23101060@usc.edu.ph>
2024-12-08 01:03:50 +0800

    very nice. editing and preview done. continue with uploading

--- niche/public/js/posteditor.js
+++ niche/public/js/posteditor.js
@@ -1,96 +1,54 @@
 const editor = document.getElementById('editor');
-const addImageBtn = document.getElementById('addImageBtn');
 const blogForm = document.getElementById('blogForm');
 var index = 0;
 
-addImageBtn.addEventListener('click', () => {
-    const newInput = document.createElement('input');
-    newInput.type = 'file';
-    newInput.accept = 'image/*';
-    newInput.style.display = 'none';
+// addImageBtn.addEventListener('click', () => {
+//     const newInput = document.createElement('input');
+//     newInput.type = 'file';
+//     newInput.accept = 'image/*';
+//     newInput.style.display = 'none';
 
-    newInput.addEventListener("change", (event) => {
-        var file = event.target.files[0];
-        if (file) {
-            newInput.name = `image_${index}`;
-            // newInput.id=`image_${index}`;   //use this id, add event listener to delete input field when user deletes image from the content.
+//     newInput.addEventListener("change", (event) => {
+//         var file = event.target.files[0];
+//         if (file) {
+//             newInput.name = `image_${index}`;
+//             // newInput.id=`image_${index}`;   //use this id, add event listener to delete input field when user deletes image from the content.
 
-            const blobUrl = URL.createObjectURL(file);
+//             const blobUrl = URL.createObjectURL(file);
 
-            const wrapperDiv = document.createElement('div');
-            editor.appendChild(wrapperDiv);
+//             const wrapperDiv = document.createElement('div');
+//             editor.appendChild(wrapperDiv);
 
-            const image = document.createElement('img');
-            image.className = 'uploaded-image';
-            image.dataset.image = `${index}`;
-            image.src = blobUrl;
-            wrapperDiv.appendChild(image);
+//             const image = document.createElement('img');
+//             image.className = 'uploaded-image';
+//             image.dataset.image = `${index}`;
+//             image.src = blobUrl;
+//             wrapperDiv.appendChild(image);
 
-            const newLine = document.createElement('div');
-            const br = document.createElement('br');
-            newLine.appendChild(br);
-            editor.appendChild(newLine);
+//             const newLine = document.createElement('div');
+//             const br = document.createElement('br');
+//             newLine.appendChild(br);
+//             editor.appendChild(newLine);
 
-            editor.focus();
-            const range = document.createRange();
-            const selection = window.getSelection();
-            range.setStartAfter(newLine);
-            range.setEndAfter(newLine);
-            selection.removeAllRanges();
-            selection.addRange(range);
+//             editor.focus();
+//             const range = document.createRange();
+//             const selection = window.getSelection();
+//             range.setStartAfter(newLine);
+//             range.setEndAfter(newLine);
+//             selection.removeAllRanges();
+//             selection.addRange(range);
 
-            blogForm.appendChild(newInput);
-            alert("Dynamic image form added properly with the index: " + index);
-            index++;
-        } else {
-            alert("ERROR:file_not_selected. New input form was not created.");
-            // newInput.remove();
-        }
-    }, { once: true });
+//             blogForm.appendChild(newInput);
+//             alert("Dynamic image form added properly with the index: " + index);
+//             index++;
+//         } else {
+//             alert("ERROR:file_not_selected. New input form was not created.");
+//             // newInput.remove();
+//         }
+//     }, { once: true });
 
-    newInput.click();
-});
-
-const previewModal = document.getElementById('previewModal');
-const titleInput = document.getElementById('title');
-const preview = document.getElementById('preview');
-
-previewModal.addEventListener('show.bs.modal', function () {    //constructs modal before opening
-    if (titleInput.value === "") {
-        const today = new Date();
-        titleInput.value = "My blog " + today.toISOString().split('T')[0];
-    }
-    preview.innerHTML = editor.innerHTML;
-});
-
-const togglePreview = document.getElementById('togglePreview');
-const modalSize = document.getElementById('modal-sizing');
-
-togglePreview.addEventListener('click', function(){
-    if(modalSize.classList.contains('laptopPreview')){
-        modalSize.classList.remove('laptopPreview');
-        modalSize.classList.add('mobilePreview');
-    } else {
-        modalSize.classList.add('laptopPreview');
-    }
-});
-
-const content = document.getElementById('content');
-
-function submitForm() {
-    const appendTitleInput = document.createElement('input');
-    appendTitleInput.value = titleInput.value;
-    appendTitleInput.type='text';
-    appendTitleInput.name='title';
-    appendTitleInput.style.display='none';
-    blogForm.appendChild(appendTitleInput);
-    
-    content.value = editor.innerHTML;
-    
-    //in the content.value, change the src value for every image, and somehow replace it with the filepath after storing.
-    // alert(appendTitleInput.value);
-    // blogForm.submit();
-}
+//     newInput.click();
+// });
 
 function createTextSection(event){
     const wrapperAddSection = event.target.closest('.wrapper-add-section');
@@ -101,11 +59,117 @@ function createTextSection(event){
     const textSection = document.createElement('div');
     textSection.classList.add('text-section');
     textSection.contentEditable='true';
-    textSection.innerHTML='Lorem Ipsum...';
+    textSection.innerHTML='Text Here';
     section.appendChild(textSection);
 
     const newWrapper = wrapperAddSection.cloneNode(true);
 
+    // const wrapper = document.createElement('div');
+    // wrapper.classList.add('wrapper-add-section');
+    // section.appendChild(wrapper);
+    
+    // const addSection = document.createElement('div');
+    // addSection.classList.add('add-section');
+    // wrapper.appendChild(addSection);
+    
+    // addSection.appendChild(document.createElement('hr'));
+    
+    // const textBtn = document.createElement('button');
+    // textBtn.classList.add('btn', 'btn-light', 'rounded-circle', 'm-1');
+    // addSection.appendChild(textBtn);
+    // const textImg = document.createElement('img');
+    // textImg.src=textBoxURL;
+    // textBtn.appendChild(textImg);
+    
+    // const imgBtn = document.createElement('button');
+    // imgBtn.classList.add('btn', 'btn-light', 'rounded-circle', 'm-1');
+    // addSection.appendChild(imgBtn);
+    // const imageImg = document.createElement('img');
+    // imageImg.src=imageBoxURL;
+    // imgBtn.appendChild(imageImg);
+    
+    // addSection.appendChild(document.createElement('hr'));
+    
+    // editor.appendChild(section);
+    // clickedSection.parentElement.insertBefore(section, clickedSection.nextElementSibling);
+    wrapperAddSection.parentElement.insertBefore(newWrapper, wrapperAddSection.nextElementSibling);
+    wrapperAddSection.parentElement.insertBefore(section, wrapperAddSection.nextElementSibling);
+    // alert("pressed");
+}
+
+function createMarkedSection(event){
+    const wrapperAddSection = event.target.closest('.wrapper-add-section');
+
+    const section = document.createElement('div');
+    section.classList.add('section');
+    
+    const markedSection = document.createElement('div');
+    markedSection.classList.add('marked-section');
+    markedSection.contentEditable='plaintext-only';
+    markedSection.innerHTML='Markdown here';
+    section.appendChild(markedSection);
+
+    const newWrapper = wrapperAddSection.cloneNode(true);
+    wrapperAddSection.parentElement.insertBefore(newWrapper, wrapperAddSection.nextElementSibling);
+    wrapperAddSection.parentElement.insertBefore(section, wrapperAddSection.nextElementSibling);
+}
+
+function createImageSection(event){
+    const wrapperAddSection = event.target.closest('.wrapper-add-section');
+    
+    const section = document.createElement('div');
+    section.classList.add('section');
+    
+    const imgSection = document.createElement('div');
+    imgSection.classList.add('image-section');
+    section.appendChild(imgSection);
+    // const imgContainer1 = document.createElement('div');
+    // imgContainer1.classList.add('image-container');
+    // imgContainer1.dataset.imageId = `${index++}`;
+    // imgSection.appendChild(imgContainer1);
+    // const plusSign1 = document.createElement('img');
+    // plusSign1.classList.add('image-container-plus');
+    // plusSign1.src = plusSignURL;
+    // imgContainer1.appendChild(plusSign1);
+    addImageContainer(imgSection);
+    // const imgContainer2 = document.createElement('div');
+    // imgContainer2.classList.add('image-container');
+    // imgContainer2.dataset.imageId = `${index++}`;
+    // imgSection.appendChild(imgContainer2);
+    // const plusSign2 = document.createElement('img');
+    // plusSign2.classList.add('image-container-plus');
+    // plusSign2.src = plusSignURL;
+    // imgContainer2.appendChild(plusSign2);
+    addImageContainer(imgSection);
+    const newWrapper = wrapperAddSection.cloneNode(true);
+
+    // const imageInput1 = document.createElement('input');
+    // imageInput1.type = 'file';
+    // imageInput1.accept = 'image/*';
+    // imageInput1.id = `imageInput_${imgContainer1.dataset.imageId}`;
+    // imageInput1.name = `image_${imgContainer1.dataset.imageId}`;
+    // imageInput1.style.display = 'none';
+    // blogForm.appendChild(imageInput1);
+
+    // const imageInput2 = document.createElement('input');
+    // imageInput2.type = 'file';
+    // imageInput2.accept = 'image/*';
+    // imageInput2.id = `imageInput_${imgContainer2.dataset.imageId}`;
+    // imageInput2.name = `image_${imgContainer2.dataset.imageId}`;
+    // imageInput2.style.display = 'none';
+    // blogForm.appendChild(imageInput2);
+
+    // imageInput1.addEventListener('change', (event) =>{
+    //     var file = event.target.files[0];
+    //     if (file) {
+    //         newInput.name = `image_${index}`;
+    //         // newInput.id=`image_${index}`;   //use this id, add event listener to delete input field when user deletes image from the content.
+
+    //         const blobUrl = URL.createObjectURL(file);
+    //     } else {
+    //         console.log("ERROR: NO FILE SELECTED");
+    //     }
+    // });
     // const wrapper = document.createElement('div');
     // wrapper.classList.add('wrapper-add-section');
     // section.appendChild(wrapper);
@@ -137,11 +201,130 @@ function createTextSection(event){
     // clickedSection.parentElement.insertBefore(section, clickedSection.nextElementSibling);
     wrapperAddSection.parentElement.insertBefore(newWrapper, wrapperAddSection.nextElementSibling);
     wrapperAddSection.parentElement.insertBefore(section, wrapperAddSection.nextElementSibling);
-    alert("pressed");
+    // alert("pressed");
 }
 
-function createImageSection(x){
+function addImageContainer(imgSection){
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('image-container');
+    imgContainer.dataset.imageId = `${index++}`;
+    imgSection.appendChild(imgContainer);
+    const plusSign = document.createElement('img');
+    plusSign.classList.add('image-container-plus');
+    plusSign.src = plusSignURL;
+    imgContainer.appendChild(plusSign);
 
+    const imageInput = document.createElement('input');
+    imageInput.type = 'file';
+    imageInput.accept = 'image/*';
+    imageInput.id = `imageInput_${imgContainer.dataset.imageId}`;
+    imageInput.name = `image_${imgContainer.dataset.imageId}`;
+    imageInput.style.display = 'none';
+    blogForm.appendChild(imageInput);
+
+    imageInput.addEventListener('change', (event) =>{
+        var file = event.target.files[0];
+        if (file) {
+            const blobUrl = URL.createObjectURL(file);
+            imgContainer.classList.add('image-container-uploaded');
+            imgContainer.classList.remove('image-container');
+            plusSign.remove();
+            const uploadedImg = document.createElement('img');
+            uploadedImg.classList.add('uploaded-image');
+            uploadedImg.src = blobUrl;
+            imgContainer.appendChild(uploadedImg);
+        } else {
+            console.log("ERROR: NO FILE SELECTED");
+        }
+    });
+}
+
+editor.addEventListener('click', function(event) {
+    if (event.target.matches('.image-container')) {
+        const imageId = event.target.dataset.imageId;
+        
+        let existingInput = document.getElementById(`imageInput_${imageId}`);
+        
+        if (existingInput) {
+            existingInput.click();
+        } else {
+            console.log("ERROR: NO SUCH INPUT FOUND" + `imageInput_${imageId}`);
+        }
+    }
+});
+
+const previewModal = document.getElementById('previewModal');
+const titleInput = document.getElementById('title');
+const preview = document.getElementById('preview');
+
+previewModal.addEventListener('show.bs.modal', function () {    //constructs modal before opening
+    if (titleInput.value === "") {
+        const today = new Date();
+        titleInput.value = "My blog " + today.toISOString().split('T')[0];
+    }
+    preview.innerHTML = '';
+    const sections = editor.querySelectorAll('.section');
+    sections.forEach(section => {
+        let append = false;
+        const clonedSection = section.cloneNode(true);
+        const clonedTextSection = clonedSection.querySelector('.text-section');
+        if(clonedTextSection){
+                clonedTextSection.contentEditable = 'false';
+                clonedTextSection.innerHTML = DOMPurify.sanitize(clonedTextSection.innerHTML);
+        } else {
+            const clonedMarkedSection = clonedSection.querySelector('.marked-section');
+            if(clonedMarkedSection){
+                    clonedMarkedSection.contentEditable = 'false';
+                    clonedMarkedSection.innerHTML = DOMPurify.sanitize(marked.marked(clonedMarkedSection.innerHTML));
+            } else {
+                const clonedImgSection = clonedSection.querySelector('.image-section');
+                const clonedUploadedImg = clonedImgSection.querySelectorAll('.image-container-uploaded');
+                if(clonedUploadedImg.length !== 0){
+                    append = true;
+                    const clonedImgContainer = clonedImgSection.querySelectorAll('.image-container');
+                    clonedImgContainer.forEach(imgContainer =>{
+                        imgContainer.classList.add('image-container-empty');
+                        imgContainer.classList.remove('image-container');
+                        imgContainer.innerHTML = '';
+                        alert('debug');
+                    })
+                }
+            }
+        }
+
+        if(append){
+            preview.appendChild(clonedSection);
+        }
+    });
+});
+
+const togglePreview = document.getElementById('togglePreview');
+const modalSize = document.getElementById('modal-sizing');
+
+togglePreview.addEventListener('click', function(){
+    if(modalSize.classList.contains('laptopPreview')){
+        modalSize.classList.remove('laptopPreview');
+        modalSize.classList.add('mobilePreview');
+    } else {
+        modalSize.classList.add('laptopPreview');
+    }
+});
+
+const content = document.getElementById('content');
+
+function submitForm() {
+    const appendTitleInput = document.createElement('input');
+    appendTitleInput.value = titleInput.value;
+    appendTitleInput.type='text';
+    appendTitleInput.name='title';
+    appendTitleInput.style.display='none';
+    blogForm.appendChild(appendTitleInput);
+    
+    content.value = editor.innerHTML;
+    
+    //in the content.value, change the src value for every image, and somehow replace it with the filepath after storing.
+    // alert(appendTitleInput.value);
+    // blogForm.submit();
 }
 
 function showCurr() {
--- niche/resources/views/posteditor.blade.php
+++ niche/resources/views/posteditor.blade.php
@@ -16,16 +16,13 @@
             <div class="add-section">
                 <hr>
                 <button type="button" onclick="createTextSection(event)" class="btn btn-light rounded-circle m-1"><img src="{{ asset('pics/textBox.svg') }}"></button>
-                <button type="button" class="btn btn-light rounded-circle m-1"><img src="{{ asset('pics/imgBox.svg') }}"></button>
+                <button type="button" onclick="createImageSection(event)" class="btn btn-light rounded-circle m-1"><img src="{{ asset('pics/imgBox.svg') }}"></button>
+                <button type="button" onclick="createMarkedSection(event)" class="btn btn-light rounded-circle m-1"><img src="{{ asset('pics/markdown.svg') }}"></button>
                 <hr>
             </div>
         </div>
     </div>
-    <button type="button" class="btn btn-light" id="addImageBtn">
-        <img class="add-image" src="{{ asset('pics/addImage.svg') }}"></img>
-    </button>
-    <button type="button" id="showCurrentContent" onclick="showCurr()">Show</button>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#previewModal">
+    <button type="button" class="btn btn-primary m-5" data-bs-toggle="modal" data-bs-target="#previewModal">
         Post Blog
     </button>
     <form id="blogForm" method="POST" action="{{ route('saveBlog.submit') }}">
@@ -35,10 +32,10 @@
     @include('components/posteditor_preview_modal')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/posteditor.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/marked/15.0.3/lib/marked.umd.min.js" integrity="sha512-Kt7350NAX8m05J2xcXWI1kAyZQjLm5c/yW0tz5qOpPyKhaidt+p5axcaL3TCaoYfJ11eHOO8nXKtXyGo+z5PMg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>    <script src="{{ asset('js/posteditor.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dompurify/3.2.2/purify.min.js" integrity="sha512-aMwmSY1jtPTUuu81C/rDUHoj2IyPpqqSX6N+efBFVFIe5nV4ZsKebsEWDUxsVdDMy3XVhY4TsZ3WHgXmhSufjw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
-        const textBoxURL = "{{ asset('pics/textBox.svg') }}";
-        const imageBoxURL = "{{asset('pics/imgBox.svg') }}";
+        const plusSignURL = "{{ asset('pics/plusSign.svg') }}";
     </script>
 </body>
 </html>
