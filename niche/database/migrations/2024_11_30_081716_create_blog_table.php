452e2f620a60c5f7cb5c6d05a793ebbda64c3590
Myk Roble <mykroble@gmail.com>
2024-12-15 23:06:09 +0800

    forgor something

--- niche/database/migrations/2024_11_30_081716_create_blog_table.php
+++ niche/database/migrations/2024_11_30_081716_create_blog_table.php
@@ -65,6 +65,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('user_channels');
+        Schema::dropIfExists('channel');
         Schema::dropIfExists('comments');
         Schema::dropIfExists('blog_images');
         Schema::dropIfExists('blogs');
